3d10fe6b223ecf44d6580f54910f372cedff5214
john <john4pius@gmail.com>
2020-07-14 17:09:39 +0100

    add setup scrit. this will be used to set environment variables

---
+++ setup.sh
@@ -0,0 +1,9 @@
+export AUTH0_DOMAIN=capstone-fsndp.us.auth0.com
+export API_AUDIENCE=moviecast
+export ALGORITHMS=['RS256']
+export DATABASE_URL=postgresql://postgres:agency@localhost:5432/boxoffice
+
+
+export CASTING_ASSISTANT_TOKEN=
+export CASTING_DIRECTOR_TOKEN=
+export EXECUTIVE_PRODUCER_TOKEN=
