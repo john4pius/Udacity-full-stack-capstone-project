68be3b80005288d4e378f01731e3e57414241047
john <john4pius@gmail.com>
2020-07-14 16:23:56 +0100

    update test case for movie

--- test_app.py
+++ test_app.py
@@ -39,3 +39,97 @@ class BoxofficeTestCase(unittest.TestCase):
             self.db.init_app(self.app)
             # create all tables
             self.db.create_all()
+
+
+
+    ''' get movies '''
+
+    def test_get_movies(self):
+        response = self.client().get("/movies", headers=casting_assistant_auth_header)
+        data = json.loads(response.data)
+
+        self.assertEqual(response.status_code, 200)
+        self.assertTrue(data["success"])
+        self.assertEqual(len(data["movies"]), 1)
+
+    def test_get_movies_not_authorized(self):
+        response = self.client().get("/movies")
+
+        data = json.loads(response.data)
+        self.assertEqual(response.status_code, 401)
+
+
+    ''' create movies '''
+
+    def test_create_movie(self):
+        new_movie = {
+            "title": "Titanic",
+            "release_date": date.today()
+        }
+
+        response = self.client().post("/movies", json=new_movie,
+                                      headers=executive_producer_auth_header)
+        data = json.loads(response.data)
+
+        self.assertEqual(response.status_code, 200)
+        self.assertTrue(data["success"])
+
+    def test_create_movie_not_authorized(self):
+        new_movie = {
+            "title": "Titanic",
+            "release_date": date.today()
+        }
+
+        response = self.client().post("/movies", json=new_movie,
+                                      headers=casting_assistant_auth_header)
+        data = json.loads(response.data)
+
+        self.assertEqual(response.status_code, 401)
+        self.assertFalse(data["success"])
+
+
+    ''' update movies '''
+
+    def test_update_movie(self):
+        update_movie = {"title": "Avatar"}
+        response = self.client().patch("/movies/1", json=update_movie,
+                                       headers=casting_director_auth_header)
+        data = json.loads(response.data)
+
+        self.assertEqual(response.status_code, 200)
+        self.assertTrue(data["success"])
+
+    def test_update_movie_unauthorized(self):
+        update_movie = {"title": "Avatar"}
+        response = self.client().patch("/movies/1", json=update_movie,
+                                       headers=casting_assistant_auth_header)
+
+        
+        data = json.loads(response.data)
+        print(data)
+        self.assertEqual(response.status_code, 401)
+        self.assertFalse(data["success"])
+
+    
+    ''' delete movie '''
+
+    def test_delete_movie(self):
+        response = self.client().delete("/movies/1", headers=executive_producer_auth_header)
+
+        data = json.loads(response.data)
+        self.assertEqual(response.status_code, 200)
+        self.assertTrue(data["success"])
+
+    def test_delete_movie_unauthorized(self):
+        response = self.client().delete("/movies/1", headers=casting_assistant_auth_header)
+
+        data = json.loads(response.data)
+        self.assertEqual(response.status_code, 401)
+        self.assertFalse(data["success"])
+
+    def test_delete_movie_not_found(self):
+        response = self.client().delete(
+            "/movies/5000", headers=executive_producer_auth_header)
+
+        data = json.loads(response.data)
+        self.assertEqual(response.status_code, 404)
